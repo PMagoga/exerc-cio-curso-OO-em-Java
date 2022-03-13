1d98261cdddc84e09d7ce6a490846667cfb28b5e
Pablo <94563183+PMagoga@users.noreply.github.com>
2022-03-13 19:27:17 -0300

    Add files via upload

---
+++ Course/src/aplication/Program.java
@@ -0,0 +1,49 @@
+package aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Product product = new Product();
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		product.name = sc.nextLine();
+		System.out.print("Price: ");
+		product.price = sc.nextDouble();
+		System.out.print("Quantity in stock: ");
+		product.quantity = sc.nextInt();
+		
+		//após adicionar o produto, quantidade e valor unitário, o programa devolve a situação do estoque
+		System.out.println();
+		System.out.println("Product data: " + product);
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		//atualiza a situação do estoque
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		//devolve a situação do estoque após remover itens do produto
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		sc.close();
+
+	}
+
+}
